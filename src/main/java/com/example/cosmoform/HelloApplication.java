2a0657efa571fff6b0b8fb3ccf20a4ec6782f7a9
techstudent <techstudent@webdev23.local>
2026-05-11 10:22:26 -0400

    idk

--- src/main/java/com/example/cosmoform/HelloApplication.java
+++ src/main/java/com/example/cosmoform/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.cosmoform;
+ package com.example.cosmoform;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cosmo.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +20,4 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
-} 
+}
